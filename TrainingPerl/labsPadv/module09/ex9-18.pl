37e17e61cead9061c96d9aaeb7a4342bd087965b
Vlad-win <k2emailbox@gmail.com>
2014-01-21 11:05:44 +0200

    perl changes

--- TrainingPerl/labsPadv/module09/ex9-18.pl
+++ TrainingPerl/labsPadv/module09/ex9-18.pl
@@ -2,7 +2,9 @@
 #ex9-18
 if (open(CHILDHANDLE, "|-")) {
 	$SIG{INT} = sub {print "Got the message.\n";};
+	sleep 2;
 	print CHILDHANDLE "$$";
+	sleep 2;
 	close (CHILDHANDLE);
 } else {
 	chomp ($parentid = <>);
